4a876c19bc442880d6444e087e61981cc3536620
Andreas Nienkötter <nienkoetter@scu.edu.cn>
2025-02-24 01:02:37 +0800

    Complete rework of niri/taskbar. Clicking missing

--- include/modules/niri/taskbar.hpp
+++ include/modules/niri/taskbar.hpp
@@ -1,13 +1,20 @@
 #pragma once
 
+#include <ranges>
 #include <gtkmm/button.h>
 #include <json/value.h>
 
+#include "AAppIconLabel.hpp"
 #include "AModule.hpp"
 #include "bar.hpp"
 #include "modules/niri/backend.hpp"
+#include "util/rewrite_string.hpp"
+#include "util/sanitize_str.hpp"
 
 namespace waybar::modules::niri {
+
+
+class Task;
 
 class Taskbar : public AModule, public EventHandler {
  public:
@@ -16,16 +23,29 @@ class Taskbar : public AModule, public EventHandler {
   void update() override;
 
  private:
+
+
   void onEvent(const Json::Value &ev) override;
   void doUpdate();
-  Gtk::Button &addButton(const Json::Value &win);
+  Gtk::Button &addTask(const Json::Value &win);
   std::string getIcon(const Json::Value &win);
+  bool image_load_icon(Gtk::Image &image, const Glib::RefPtr<Gtk::IconTheme> &icon_theme,
+      Glib::RefPtr<Gio::DesktopAppInfo> app_info, int size);
 
   const Bar &bar_;
   Gtk::Box box_;
 
-  // Map from niri window id to button
-  std::unordered_map<uint64_t, Gtk::Button> buttons_;
+  // Map from niri window id to task
+  std::unordered_map<uint64_t, std::shared_ptr<Task>> tasks_;
+};
+
+class Task : public AAppIconLabel {
+  public:
+    Task(const Json::Value &win, const Json::Value &config);
+    ~Task() override;
+    auto doUpdate(const Json::Value &win, std::unordered_map<uint64_t, const Json::Value> my_workspaces, const Bar &bar_) -> void;
+    auto update() -> void override;
+    Gtk::EventBox &getEventBox();
 };
 
 }  // namespace waybar::modules::niri
--- src/modules/niri/taskbar.cpp
+++ src/modules/niri/taskbar.cpp
@@ -36,111 +36,77 @@ void Taskbar::onEvent(const Json::Value &ev) { dp.emit(); }
 void Taskbar::doUpdate() {
   auto ipcLock = gIPC->lockData();
 
-  const auto alloutputs = config_["all-outputs"].asBool();
-  const auto allworkspaces = config_["all-workspaces"].asBool();
-  std::vector<Json::Value> my_windows;
-  std::vector<Json::Value> my_workspaces;
+  /* std::vector<Json::Value> my_windows; */
+  std::unordered_map<uint64_t, const Json::Value> my_workspaces;
   const auto &workspaces = gIPC->workspaces();
   const auto &windows = gIPC->windows();
 
+  /* insert workspaces into map for visibility checking */
+  for (const Json::Value &ws: workspaces)
+    my_workspaces.insert(std::pair<uint64_t, const Json::Value>(ws["id"].asUInt64(), ws));
 
-  // get active workspace on current output
-  auto current_ws = std::find_if(workspaces.cbegin(), workspaces.cend(),
-      [&](const auto &ws) {
-        return ws["output"].asString() == bar_.output->name && ws["is_active"].asBool();
-      });
 
-  // get all relevant windows on the current workspace
-  std::copy_if(windows.cbegin(), windows.cend(), std::back_inserter(my_windows),
-               [&](const auto &win) {
-                 if (allworkspaces) return true;
+  /* get all workspaces for which windows should be shown */
+  /* auto current_ws = std::views::filter(workspaces, */
+  /*     [&](const auto &ws) { */
+  /*     if (config_["all-outputs"] && config_["all-workspaces"]) */
+  /*       return true; */
+  /*  */
+  /*     if (config_["all-outputs"]) */
+  /*       return ws["is_active"].asBool(); */
+  /*  */
+  /*     return ws["is_active"].asBool() && ws["output"].asString() == bar_.output->name; */
+  /*     }); */
 
-                 return win["workspace_id"].asUInt64() == (*current_ws)["id"].asUInt64();
-               });
+  // get all relevant windows on the selected workspaces
+  /* std::ranges::copy_if(windows, std::back_inserter(my_windows), */
+  /*              [&](const auto &win) { */
+  /*                return std::ranges::find_if(*current_ws, */
+  /*                    [&](const auto &ws) { return win["workspace_id"].asUInt64() == ws["id"].asUInt64(); }) != */
+  /*                current_ws->end(); */
+  /*              }); */
 
-  // Remove buttons for removed windows.
-  for (auto it = buttons_.begin(); it != buttons_.end();) {
+  // Remove tasks for removed windows.
+  for (auto it = tasks_.begin(); it != tasks_.end();) {
     // find window id of this button in window list
-    auto win = std::find_if(my_windows.begin(), my_windows.end(),
+    auto win = std::ranges::find_if(windows,
                            [it](const auto &win) { return win["id"].asUInt64() == it->first; });
+
     // remove if not found
-    if (win == my_windows.end()) {
-      it = buttons_.erase(it);
+    if (win == windows.end()) {
+      it = tasks_.erase(it);
     } else {
       ++it;
     }
   }
 
-  // Add buttons for new windows, update existing ones.
-  // TODO
-  for (const auto &win : my_windows) {
-    auto bit = buttons_.find(win["id"].asUInt64());
-    auto &button = bit == buttons_.end() ? addButton(win) : bit->second;
-    auto style_context = button.get_style_context();
+  // Add tasks for new windows, update existing ones.
+  for (const auto &win : windows) {
 
-    // add and remove style classes based on window type
-    if (win["is_focused"].asBool())
-      style_context->add_class("focused");
+    auto ti = tasks_.find(win["id"].asUInt64());
+    Task *task;
+    if (ti == tasks_.end()) {
+      tasks_.emplace(win["id"].asUInt64(), std::make_shared<Task>(win, config_));
+      task = tasks_.find(win["id"].asUInt64())->second.get();
+      box_.add(task->getEventBox());
+    }
     else
-      style_context->remove_class("focused");
+      task = ti->second.get();
 
-    if (win["is_floating"].asBool())
-      style_context->add_class("floating");
-    else
-      style_context->remove_class("floating");
-
-    if (win["workspace_id"]) {
-      if (win["workspace_id"].asUInt64() == (*current_ws)["id"].asUInt64())
-        style_context->add_class("current_workspace");
-      else
-        style_context->remove_class("current_workspace");
-    } else {
-      style_context->remove_class("current_workspace");
-    }
-
-    // handle named workspaces
-    // TODO: handle named windows!
-    std::string title = win["title"].asString();
-    button.set_name("niri-window-" + win["id"].asString());
-
-    if (config_["format"].isString()) {
-      auto format = config_["format"].asString();
-      title = fmt::format(fmt::runtime(format), fmt::arg("icon", getIcon(win)),
-                         fmt::arg("appid", win["app_id"].asString()),
-                         fmt::arg("value", title), fmt::arg("title", win["title"].asString()),
-                         fmt::arg("windowid", win["id"].asUInt()),
-                         fmt::arg("workspace", win["workspace_id"].asString()));
-    }
-    if (!config_["disable-markup"].asBool()) {
-      static_cast<Gtk::Label *>(button.get_children()[0])->set_markup(title);
-    } else {
-      button.set_label(title);
-    }
-
-    // hide windows
-    // TODO hide windows based on list?
-    if (config_["current-only"].asBool()) {
-      const auto *property = alloutputs ? "is_focused" : "is_active";
-      if (win[property].asBool())
-        button.show();
-      else
-        button.hide();
-    } else {
-      button.show();
-    }
+    task->doUpdate(win, my_workspaces, bar_);
   }
 
   // Refresh the button order.
-  for (auto it = my_windows.cbegin(); it != my_windows.cend(); ++it) {
-    const auto &ws = *it;
+  /* for (auto it = my_windows.cbegin(); it != my_windows.cend(); ++it) { */
+  /*   const auto &ws = *it; */
+  /*  */
+  /*   auto pos = ws["id"].asUInt() - 1; */
+  /*  */
+  /*   auto &button = tasks_[ws["id"].asUInt64()].button; */
+  /*   box_.reorder_child(button, pos); */
+  /* } */
 
-    // TODO sort by position?
-    auto pos = ws["id"].asUInt() - 1;
-    if (alloutputs) pos = it - my_windows.cbegin();
-
-    auto &button = buttons_[ws["id"].asUInt64()];
-    box_.reorder_child(button, pos);
-  }
+  spdlog::debug("[Taskbar]: updated.");
 }
 
 void Taskbar::update() {
@@ -148,39 +114,116 @@ void Taskbar::update() {
   AModule::update();
 }
 
-Gtk::Button &Taskbar::addButton(const Json::Value &win) {
-  std::string name = win["id"].asString();
+/* Task implementations */
+Task::Task(const Json::Value &win, const Json::Value &config):
+  AAppIconLabel(config, "task", win["app_id"].asString(), config["format"].asString()) {
+    event_box_.show_all();
+    box_.set_name("task");
+};
 
-  auto pair = buttons_.emplace(win["id"].asUInt64(), name);
-  auto &&button = pair.first->second;
-  box_.pack_start(button, false, false, 0);
-  button.set_relief(Gtk::RELIEF_NONE);
-  if (!config_["disable-click"].asBool()) {
-    const auto id = win["id"].asUInt64();
-    button.signal_pressed().connect([=] {
-      try {
-        // {"Action":{"FocusWindow":{"id":1}}}
-        Json::Value request(Json::objectValue);
-        auto &action = (request["Action"] = Json::Value(Json::objectValue));
-        auto &focusWindow = (action["FocusWindow"] = Json::Value(Json::objectValue));
-        focusWindow["id"] = id;
 
-        IPC::send(request);
-      } catch (const std::exception &e) {
-        spdlog::error("Error switching window: {}", e.what());
-      }
-    });
+Task::~Task() = default;
+
+
+auto Task::doUpdate(const Json::Value &win, const std::unordered_map<uint64_t, const Json::Value> my_workspaces, const Bar &bar_) -> void {
+
+  /* visibility */
+  const auto &ws = my_workspaces.find(win["workspace_id"].asUInt64())->second;
+
+  if ((config_["all-outputs"].asBool() && config_["all-workspaces"].asBool()) ||
+      (config_["all-outputs"].asBool() && ws["is_active"].asBool()) ||
+      (config_["all-workspaces"].asBool() && ws["output"].asString() == bar_.output->name) ||
+      (ws["is_active"].asBool() && ws["output"].asString() == bar_.output->name)
+      )
+    event_box_.show();
+  else
+    event_box_.hide();
+
+  /* format text */
+  std::string format_text = config_["format"].asString();
+
+  if (win["is_floating"].asBool() && config_["format-floating"].isString())
+    format_text = config_["format-floating"].asString();
+
+  if (win["is_focused"].asBool() && config_["format-focused"].isString())
+    format_text = config_["format-focused"].asString();
+
+  std::string label_text;
+  if (!format_text.empty()) {
+    label_.show();
+    label_text = waybar::util::rewriteString(
+        fmt::format(fmt::runtime(format_text),
+          fmt::arg("appid", win["app_id"].asString()),
+          fmt::arg("title", win["title"].asString()),
+          fmt::arg("windowid", win["id"].asString()),
+          fmt::arg("workspaceid", win["workspace_id"].asString())),
+        config_["rewrite"]);
+    label_.set_markup(label_text);
   }
-  return button;
+  else
+    label_.hide();
+
+  /* tooltip */
+  if (tooltipEnabled()) {
+    std::string tooltip_format;
+    if (config_["tooltip-format"].isString()) {
+      tooltip_format = config_["tooltip-format"].asString();
+    }
+    if (!tooltip_format.empty()) {
+      event_box_.set_tooltip_text(
+          fmt::format(fmt::runtime(tooltip_format),
+                      fmt::arg("appid", win["app_id"].asString()),
+                      fmt::arg("title", win["title"].asString()),
+                      fmt::arg("windowid", win["id"].asString()),
+                      fmt::arg("workspaceid", win["workspace_id"].asString())));
+    } else if (!label_text.empty()) {
+      event_box_.set_tooltip_text(label_text);
+    }
+  }
+  else
+    event_box_.set_tooltip_text(nullptr);
+
+  /* handle icon */
+  if (config_["icon"].asBool()) {
+    updateAppIconName(win["app_id"].asString(), win["title"].asString());
+    updateAppIcon();
+    image_.show();
+  }
+  else
+    image_.hide();
+
+  /* add and remove style classes based on window type */
+  auto style_context = box_.get_style_context();
+  if (win["is_focused"].asBool())
+    style_context->add_class("focused");
+  else
+    style_context->remove_class("focused");
+
+  if (win["is_floating"].asBool())
+    style_context->add_class("floating");
+  else
+    style_context->remove_class("floating");
+
+  if (win["workspace_id"].asBool() && ws["is_focused"].asBool())
+    style_context->add_class("focused_workspace");
+  else
+    style_context->remove_class("focused_workspace");
+
+  if (win["workspace_id"].asBool() && ws["is_active"].asBool())
+    style_context->add_class("active_workspace");
+  else
+    style_context->remove_class("active_workspace");
+
+  spdlog::debug("[Task]: {}, visible: {}, label_text: {}, tooltip_format: {}, icon_name: {}", win["app_id"].asString(), event_box_.is_visible(), label_text, config_["tooltip-format"].asString(), app_icon_name_);
+  AAppIconLabel::update();
 }
 
-std::string Taskbar::getIcon(const Json::Value &win) {
-  // TODO get icon from desktop file
-  return win["title"].asString();
+Gtk::EventBox &Task::getEventBox() {
+  return event_box_;
 }
 
-// for icons
-// handle app_id is called to get name and icon
-// - image_load_icon -> uses icon theme and app info to load icon image
+auto Task::update() -> void {
+  AAppIconLabel::update();
+}
 
 }  // namespace waybar::modules::niri
